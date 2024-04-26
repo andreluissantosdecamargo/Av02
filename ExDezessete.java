5a7547b5966449763b7b161f48e69ddf58f9cc66
André Luís <110728461+andreluissantosdecamargo@users.noreply.github.com>
2024-04-26 15:51:00 -0300

    Update ExDezessete.java

--- ExDezessete.java
+++ ExDezessete.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ExDezessete {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         float sal_carlos, sal_joao;
@@ -12,7 +12,7 @@ public class ExDezessete {
         
         sal_carlos = 0;
         meses = 0;
-  
+        
         while (sal_carlos <= 0) {
             System.out.print("Digite o salário de Carlos: ");
             sal_carlos = sc.nextFloat();
@@ -23,17 +23,20 @@ public class ExDezessete {
         }
         
         sal_joao = sal_carlos / 3;
-        
+       
         while (sal_joao < sal_carlos) {
             meses++;
             sal_carlos = sal_carlos + (sal_carlos * 2 / 100);
             sal_joao = sal_joao + (sal_joao * 5 / 100);
+            System.out.println("Mês " + meses + ":");
+            System.out.println("Valor que Carlos pertence: R$" + sal_carlos);
+            System.out.println("Valor que João pertence: R$" + sal_joao);
         }
         
-        System.out.println("\nQuantidade de meses necessários para que o salário de João seja igual ou maior que o de Carlos: " + meses);
-        System.out.println("Valor que Carlos pertence: R$" + sal_carlos);
-        System.out.println("Valor que João pertence: R$" + sal_joao);
+        	System.out.println("\nQuantidade de meses necessários para que o salário de João seja igual ou maior que o de Carlos: " + meses);
+        	System.out.println("Valor que Carlos pertence: R$" + sal_carlos);
+        	System.out.println("Valor que João pertence: R$" + sal_joao);
         
         sc.close();
-	    }
-	}
+    }
+}
